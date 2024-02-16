d1394f94e56bd3f753df288ec4879f8a47ab5128
Rahul <rajamohanty12345@gmail.com>
2024-02-16 14:25:26 +0530

    build fix 1

---
+++ backend/dist/app.js
@@ -0,0 +1,29 @@
+"use strict";
+
+const express = require("express");
+const {
+  createServer
+} = require("http");
+const {
+  Server
+} = require("socket.io");
+const cors = require("cors");
+const socketController = require("./controllers/socketController");
+const app = express();
+//for cross origin
+app.use(cors());
+
+//for server creation
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: "*"
+  }
+});
+
+//for socket instance
+socketController(io);
+
+//for server listen
+httpServer.listen(8000);
+module.exports = app;
